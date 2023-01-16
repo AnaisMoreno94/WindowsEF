1c3d13d2616e00a3f161cd44d03a2b6e231a5aa7
Anais Moreno <anabet.94@gmail.com>
2023-01-16 15:31:16 -0300

    Agregar archivos de proyecto.

---
+++ WindowsEF/Data/DBProductosBootcampContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsEF.Models;
+
+namespace WindowsEF.Data
+{
+    public class DBProductosBootcampContext:DbContext 
+    {
+        //Constructor
+        public DBProductosBootcampContext():base("KeyDB"){}
+        //Propiedades  DBSET 
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Producto> Productos { get; set; }
+    }
+}
---
+++ WindowsEF/Models/Categoria.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsEF.Models
+{
+    public class Categoria
+    {
+        //Usa la convencion EF para indicar que en SQL Server la columna Id sea la clave principal y autonumerica
+        //Id o Categoria
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
